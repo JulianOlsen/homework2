394ebfaeca8afc3a87071433f1de9cd1b56f3e28
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 18:20:51 +0200

    Add files via upload

---
+++ homework2/src/homework2/Homework2a.java
@@ -0,0 +1,21 @@
+package homework2a.java;
+
+import java.util.Scanner;
+
+public class TestFirstLessonInCode 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("press 1 to convert from celsius to farenhight");
+        System.out.println("press 2 to convert from farenhight to celsius");
+
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        System.out.println("type a number");
+        int x = Sc.nextInt();
+
+        
+     }
+}
+
